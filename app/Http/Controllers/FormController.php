fbe74322b14360ec6a1034cd0745b283a1118206
Daniel Feo <danielandres.feo@gmail.com>
2017-09-21 15:58:03 -0500

    controlador formulario

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Ciudad;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Pais;
+use Idrd\Usuarios\Repo\Departamento;
+use Idrd\Usuarios\Repo\Localidad;
+use Idrd\Usuarios\Repo\PersonaInterface;
+use Illuminate\Http\Request;
+
+class FormController extends Controller
+{
+
+
+
+
+}
